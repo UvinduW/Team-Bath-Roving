806fc37e7a75da1e8ad31b637d0ef1a4c5755955
Varun-Chhabra <37084327+Varun-Chhabra@users.noreply.github.com>
2018-03-15 15:15:32 +0000

    Added Video Stream Code (Client Side)

--- PC/pc_development_files/pc_gamepad_transmission/pc_gamepad_transmission.py
+++ PC/pc_development_files/pc_gamepad_transmission/pc_gamepad_transmission.py
@@ -1,8 +1,9 @@
 # pc_gamepad_transmission.py
 # Created: 2017.12.03
-# Uvindu Wijesinghe and Oliver Wilkins
+# Uvindu Wijesinghe, Oliver Wilkins, and Varun Chhabra
 #
 # Note: Code adapted from Project Intelligent Car & RCAutopilot
+# Video Stream Code adapted from http://picamera.readthedocs.io/en/release-1.12/recipes1.html
 #
 # Description:
 # A program to receive gamepad input and send it over a TCP socket
@@ -17,7 +18,41 @@
 import socket
 import pygame
 from time import sleep
+######################################################### Video Stream (Client Side) #########################################
+import io
+from PIL import Image
 
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))
+server_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+try:
+    while True:
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        image = Image.open(image_stream)
+        print('Image is %dx%d' % image.size)
+        image.verify()
+        print('Image is verified')
+finally:
+    connection.close()
+    server_socket.close()
+
+	##################################################################
 # Name the gamepad axes.
 axis_steer = 0
 axis_forwards = 4
