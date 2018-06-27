ab2979a8f51909a8b581a962f2eee251ef42fda5
Varun-Chhabra <37084327+Varun-Chhabra@users.noreply.github.com>
2018-06-27 04:19:31 +0100

    Concurrency
    Quick attempt at using multiprocessing

--- PC/pc_development_files/pc_gamepad_transmission/pc_gamepad_transmission.py
+++ PC/pc_development_files/pc_gamepad_transmission/pc_gamepad_transmission.py
@@ -18,6 +18,7 @@
 import socket
 import pygame
 from time import sleep
+from multiprocessing import Process
 ######################################################### Video Stream (Client Side) #########################################
 import io
 from PIL import Image
@@ -30,27 +31,28 @@ server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
-try:
-    while True:
-        # Read the length of the image as a 32-bit unsigned int. If the
-        # length is zero, quit the loop
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
-        if not image_len:
-            break
-        # Construct a stream to hold the image data and read the image
-        # data from the connection
-        image_stream = io.BytesIO()
-        image_stream.write(connection.read(image_len))
-        # Rewind the stream, open it as an image with PIL and do some
-        # processing on it
-        image_stream.seek(0)
-        image = Image.open(image_stream)
-        print('Image is %dx%d' % image.size)
-        image.verify()
-        print('Image is verified')
-finally:
-    connection.close()
-    server_socket.close()
+def video_stream()
+    try:
+        while True:
+            # Read the length of the image as a 32-bit unsigned int. If the
+            # length is zero, quit the loop
+            image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+            if not image_len:
+                break
+            # Construct a stream to hold the image data and read the image
+            # data from the connection
+            image_stream = io.BytesIO()
+            image_stream.write(connection.read(image_len))
+            # Rewind the stream, open it as an image with PIL and do some
+            # processing on it
+            image_stream.seek(0)
+            image = Image.open(image_stream)
+            print('Image is %dx%d' % image.size)
+            image.verify()
+            print('Image is verified')
+    finally:
+        connection.close()
+        server_socket.close()
 
 	##################################################################
 # Name the gamepad axes.
@@ -80,19 +82,20 @@ j_count = pygame.joystick.get_count()
 gamepad = pygame.joystick.Joystick(1)
 gamepad.init()
 
-while True:
-    # Refresh the values from gamepad
-    pygame.event.pump()
+def rover_control()
+    while True:
+        # Refresh the values from gamepad
+        pygame.event.pump()
     
-	# The gamepad analog axes and triggers provide a value between -1 and positive 1 depending on how
-	#  far they've been pushed or pressed. 0 is the center point for the thumbsticks or
-	#  half pressed for the shoulder triggers
+	    # The gamepad analog axes and triggers provide a value between -1 and positive 1 depending on how
+	    #  far they've been pushed or pressed. 0 is the center point for the thumbsticks or
+	    #  half pressed for the shoulder triggers
 	
-	# The Scaling used should be changed as required by the motor controller chosen
+	    # The Scaling used should be changed as required by the motor controller chosen
 	
-    # Get the steering angle from axis0. Round, scale, and shift it, make it a three-char string.
-	# Scaled such that 0: full left; 90: centered; 180: full right
-    steer_angle = str(int(round(gamepad.get_axis(axis_steer)*90 + 90, 0))).zfill(3)
+            # Get the steering angle from axis0. Round, scale, and shift it, make it a three-char string.
+	    # Scaled such that 0: full left; 90: centered; 180: full right
+        steer_angle = str(int(round(gamepad.get_axis(axis_steer)*90 + 90, 0))).zfill(3)
     
 	# The Xbox triggers can be used for the throttle. With PyGame and the Xbox controller
 	#  the trigger reads -1 when fully pressed or 1 when not pressed at all. Due to how 
@@ -102,28 +105,32 @@ while True:
 	#  pressed at least once.
 	
     # Latch the forwards_moved flag if the forward gamepad axis ever moves from zero.
-    if gamepad.get_axis(axis_forwards) != 0:
-        forwards_moved = 1
+        if gamepad.get_axis(axis_forwards) != 0:
+            forwards_moved = 1
 
     # Latch the reverse_moved flag if the reverse gamepad axis ever moves from zero.
-    if gamepad.get_axis(axis_reverse) != 0:
-        reverse_moved = 1
+        if gamepad.get_axis(axis_reverse) != 0:
+            reverse_moved = 1
 
     # Start calculating throttle values once the throttle has been moved once.
 	# Scaled such that 0: full speed forwards; 90: no throttle; 180: full speed reverse
-    if forwards_moved == 1:
+        if forwards_moved == 1:
         # Read the forward axis and calculate a throttle value, rounded, scaled, shifted, stringed, extended.
-        throttle = str(90 - int(round(gamepad.get_axis(axis_forwards)*45 + 45, 0))).zfill(3)
+            throttle = str(90 - int(round(gamepad.get_axis(axis_forwards)*45 + 45, 0))).zfill(3)
 
     # If the reverse trigger is pressed at all, override throttle value with this value.
-    if reverse_moved == 1 and gamepad.get_axis(axis_reverse) != -1:
+        if reverse_moved == 1 and gamepad.get_axis(axis_reverse) != -1:
         # Read the reverse axis and calculate a throttle value, rounded, scaled, shifted, stringed, extended.
-        throttle = str(90 + int(round(gamepad.get_axis(axis_reverse)*45 + 45, 0))).zfill(3)
+            throttle = str(90 + int(round(gamepad.get_axis(axis_reverse)*45 + 45, 0))).zfill(3)
 
     # Create a string to transmit over the socket by concatenating the two values.
-    transmission_string = throttle + steer_angle
+        transmission_string = throttle + steer_angle
 
     # Print and send the string.
-    print transmission_string
-    socket_control.sendto(transmission_string, (ip_address, port))
-    sleep(0.1)
+        print transmission_string
+        socket_control.sendto(transmission_string, (ip_address, port))
+        sleep(0.1)
+	
+if __name__ == '__main__':
+    Process(target=video_stream).start()
+    Process(target=rover_control).start()
