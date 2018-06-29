b8cecfd4344493fcb4bf23e173da9b9af4832649
Varun-Chhabra <37084327+Varun-Chhabra@users.noreply.github.com>
2018-06-29 19:49:50 +0100

    Concurrency
    Added concurrency using multiprocessing library

--- Raspberry Pi/rpi_development_files/rpi_rover_control_client/rpi_rover_control_client.py
+++ Raspberry Pi/rpi_development_files/rpi_rover_control_client/rpi_rover_control_client.py
@@ -20,124 +20,127 @@ import socket
 import servo # importing servo library for sampler
 import ThunderBorg
 import RPI.GPIO as GPIO
+from multiprocessing import Process
 ################################# For Video Stream ################################################
 import io
 import struct
 import time
 import picamera
 
-
+def video_stream()
 # Connect a client socket to team_bath_roving_server:8001
-client_socket = socket.socket()
-client_socket.connect(('team_bath_roving_server', 8001))
+    client_socket = socket.socket()
+    client_socket.connect(('team_bath_roving_server', 8001))
 
 # Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
-try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
+    connection = client_socket.makefile('wb')
+    try:
+        camera = picamera.PiCamera()
+        camera.resolution = (640, 480)
     # Start a preview and let the camera warm up for 2 seconds
-    camera.start_preview()
-    time.sleep(2)
+        camera.start_preview()
+        time.sleep(2)
 
     # Note the start time and construct a stream to hold image data
     # temporarily (we could write it directly to connection but in this
     # case we want to find out the size of each capture first to keep
     # our protocol simple)
-    start = time.time()
-    stream = io.BytesIO()
-    for foo in camera.capture_continuous(stream, 'jpeg'):
+        start = time.time()
+        stream = io.BytesIO()
+        for foo in camera.capture_continuous(stream, 'jpeg'):
         # Write the length of the capture to the stream and flush to
         # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
         # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
+            stream.seek(0)
+            connection.write(stream.read())
         # If we've been capturing for more than 30 seconds, quit
-        if time.time() - start > 30:
-            break
+            if time.time() - start > 30:
+                break
         # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
+            stream.seek(0)
+            stream.truncate()
     # Write a length of zero to the stream to signal we're done
-    connection.write(struct.pack('<L', 0))
-finally:
-    connection.close()
-    client_socket.close()
+        connection.write(struct.pack('<L', 0))
+    finally:
+        connection.close()
+        client_socket.close()
 
 
 ############################################################################################
+
+def rover_control()
 #Number the RPI IO pins using BOARD
-GPIO.setmode(GPIO.BOARD) 
+    GPIO.setmode(GPIO.BOARD) 
 #Set the first pin as an output
-GPIO.setup(1,GPIO.OUT)
+    GPIO.setup(1,GPIO.OUT)
 #Create a PWM instance
-sampler = GPIO.PWM(1,50)
+    sampler = GPIO.PWM(1,50)
 
 #Need to set the board address of each Thunderborg separately to addresses e.g. 10 11
 
 # Board #1, address 10, two left motors, Motor1: Front, Motor2: Rear
-TB1 = ThunderBorg.ThunderBorg()
-TB1.i2cAddress = 10
-TB1.Init()
-TB1.ResetEpo()
+    TB1 = ThunderBorg.ThunderBorg()
+    TB1.i2cAddress = 10
+    TB1.Init()
+    TB1.ResetEpo()
 # Board #2, address 11, two right motors, Motor1: Front, Motor2: Rear
-TB2 = ThunderBorg.ThunderBorg()
-TB2.i2cAddress = 11
-TB2.Init()
-TB2.ResetEpo()
+    TB2 = ThunderBorg.ThunderBorg()
+    TB2.i2cAddress = 11
+    TB2.Init()
+    TB2.ResetEpo()
 
 
 
 # Set IP and port.
-ip_address = "192.168.0.22"
-local_address = "0.0.0.0"
-port = 8000
+    ip_address = "192.168.0.22"
+    local_address = "0.0.0.0"
+    port = 8000
 
 # Create a socket object for control signal transmission.
-socket_control = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_control = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the IP and port to the socket object.
-socket_control.bind((local_address, port))
+    socket_control.bind((local_address, port))
 
-while True:
+    while True:
     # Receive control strings over the socket from the PC.
-    data, addr = socket_control.recvfrom(1024)
+        data, addr = socket_control.recvfrom(1024)
     
     # Print the messages
-    print "Message: ", data
+        print "Message: ", data
 	
 	# Use the data to control motors
 
     #Extract throttle and steer_angle from data
-    throttle = int(data[0:3])
-    steer_angle = int(data[3:6])
-    sampler_position = int(data[-3:])
+        throttle = int(data[0:3])
+        steer_angle = int(data[3:6])
+        sampler_position = int(data[-3:])
     #scale throttle, steer angle, and sampler position to range [-1,1]
-    scaled_sampler_position = (90 - sampler_position)/90 #1 is max clockise, -1 max anticlockwise
-    scaled_throttle =  (90 - throttle)/90
-    scaled_steer_angle = (steer_angle - 90)/90
+        scaled_sampler_position = (90 - sampler_position)/90 #1 is max clockise, -1 max anticlockwise
+        scaled_throttle =  (90 - throttle)/90
+        scaled_steer_angle = (steer_angle - 90)/90
 
 	
-    left_power = scaled_throttle + scaled_steer_angle
-    right_power = scaled_throttle - scaled_steer_angle
+        left_power = scaled_throttle + scaled_steer_angle
+        right_power = scaled_throttle - scaled_steer_angle
 	
 #Scale left_power and right_power so both stay in range [-1,1]	
 
-    if (left_power >= 1) or (right_power <= -1)
-        left_power  *= 1/2
-        right_power *= 1/2
+        if (left_power >= 1) or (right_power <= -1)
+            left_power  *= 1/2
+            right_power *= 1/2
 	
-    TB1.SetMotors(left_power)
-    TB2.SetMotors(right_power)
+        TB1.SetMotors(left_power)
+        TB2.SetMotors(right_power)
 
         
     #Sampler Control Code
     #Servo is neutral at 1.5ms, >1.5 Counter-Clockwise, <1.5 Clockwise
-   pulse_width = 1.5 - scaled_sampler_position
-   duty_cycle = pulse_width * 5 # this is the simplification of D = PW/T * 100 with f=1/T = 50Hz
-   sampler.ChangeDutyCycle(duty_cycle)
+       pulse_width = 1.5 - scaled_sampler_position
+       duty_cycle = pulse_width * 5 # this is the simplification of D = PW/T * 100 with f=1/T = 50Hz
+       sampler.ChangeDutyCycle(duty_cycle)
 
 
-GPIO.cleanup() #Check if neccessary
+    GPIO.cleanup() #Check if neccessary
